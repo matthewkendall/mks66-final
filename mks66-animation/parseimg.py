21703d109d2d5ca1235b0890694a8042868f546f
SashaKor <akoroza@stuy.edu>
2019-06-12 00:05:03 -0400

    flipp

--- mks66-animation/parseimg.py
+++ mks66-animation/parseimg.py
@@ -1,7 +1,7 @@
 '''
 img directory contains currently parsed texture
 takes in image and breaks up into u,v
-[0][0] at upper left
+[0][0] at lower left of image
 Note that .png images will NOT work
 '''
 from PIL import Image
@@ -18,7 +18,7 @@ def getPicData(img_file):
     pixels = list(img.getdata())
     return pixels
 
-def get2DPicData(img_file): #possibly wrong?
+def get2DPicData(img_file):
     '''Returns a 2D array of RGB tuples from opened image.
     (broken up for testing)'''
     pixels= getPicData(img_file)
@@ -48,13 +48,18 @@ def getUVDict(img_file):
     width,height= getPicSize(img_file) #serve as step values
     uv_dict={}
     dPixels= get2DPicData(img_file)
+    step = 1.0 / height #the little amt by which row num changes
+    u=1
 
     for r in range(height): #tracking row currently on
         #print(str(r))
+        #print("row "+ str(r))
         for c in range(width): #tracking column currently on
-            uv=((r+1)/height,(c+1)/width)
+            uv=(u,(c+1)/width)
             #print(str(uv))
             uv_dict[uv]= dPixels[r-1][c-1]
+
+        u -= step #used to flip to have 0,0 at lower left
 
     return uv_dict
 
@@ -69,6 +74,6 @@ Test Area
 #print(str(tiny))
 #print(str(len(tiny))) #rows
 #print(str(len(tiny[0]))) #columns
-# print(str(getUVDict('TEXTURE.jpg')))
+#print(str(len(getUVDict('tiny.jpeg'))))
 #print(str(len(get2DPicData('test.jpg')))) # rows
 #print(str(len(get2DPicData('test.jpg')[0]))) #columns
